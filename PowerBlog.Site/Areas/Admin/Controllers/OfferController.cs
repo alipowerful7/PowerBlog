39908248f630c09666f3d531c5943b1bfd0b91e4
Ali <ali7.khosrojerdi@gmail.com>
2025-03-07 00:24:58 +0330

    add: offer pay enable and disable and edit

--- PowerBlog.Site/Areas/Admin/Controllers/OfferController.cs
+++ PowerBlog.Site/Areas/Admin/Controllers/OfferController.cs
@@ -28,9 +28,9 @@ namespace PowerBlog.Site.Areas.Admin.Controllers
         public async Task<IActionResult> PostCreatWithAmount(OfferPay offerPay)
         {
             var offer = await _context.OfferPays.FirstOrDefaultAsync(x => x.OfferWord == offerPay.OfferWord);
-            if (offer!=null)
+            if (offer != null)
             {
-                TempData["ErrorMessage"]= "کد تخفیف تکراری است";
+                TempData["ErrorMessage"] = "کد تخفیف تکراری است";
                 return RedirectToAction("CreatWithAmount", "Offer", new { area = "Admin" });
             }
             offerPay.CreatDate = DateTime.Now;
@@ -53,6 +53,95 @@ namespace PowerBlog.Site.Areas.Admin.Controllers
             }
             offerPay.CreatDate = DateTime.Now;
             await _context.OfferPays.AddAsync(offerPay);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Offer", new { area = "Admin" });
+        }
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var offer = await _context.OfferPays.FirstOrDefaultAsync(o => o.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            return View(offer);
+        }
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var offer = await _context.OfferPays.FirstOrDefaultAsync(o => o.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            return View(offer);
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostEdit(long? id, OfferPay offerPay)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var offer = await _context.OfferPays.FirstOrDefaultAsync(o => o.OfferWord == offerPay.OfferWord);
+            if (offer != null)
+            {
+                TempData["ErrorMessage"] = "کد تخفیف تکراری است";
+                return RedirectToAction("Edit", "Offer", new { area = "Admin", id = id.Value });
+            }
+            offerPay.Id = id.Value;
+            _context.OfferPays.Update(offerPay);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Offer", new { area = "Admin" });
+        }
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var offer = await _context.OfferPays.FirstOrDefaultAsync(o => o.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            _context.OfferPays.Remove(offer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Offer", new { area = "Admin" });
+        }
+        public async Task<IActionResult> EnableOffer(long? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var offer = await _context.OfferPays.FirstOrDefaultAsync(o => o.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            offer.IsDisable = false;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Offer", new { area = "Admin" });
+        }
+        public async Task<IActionResult> DisableOffer(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var offer = await _context.OfferPays.FirstOrDefaultAsync(o => o.Id == id);
+            if (offer == null)
+            {
+                return NotFound();
+            }
+            offer.IsDisable = true;
             await _context.SaveChangesAsync();
             return RedirectToAction("Index", "Offer", new { area = "Admin" });
         }
