73572728a4ed7f4a32e3dfec853859c44b8d488b
Ali <ali7.khosrojerdi@gmail.com>
2025-03-10 21:21:47 +0330

    add: send email to users

---
+++ PowerBlog.Site/Areas/Admin/Controllers/SendEmailToUserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PowerBlog.Site.Attributes;
+using PowerBlog.Site.Data;
+using PowerBlog.Site.Models;
+using PowerBlog.Site.Utilities;
+
+namespace PowerBlog.Site.Areas.Admin.Controllers
+{
+    [AdminAuthorize]
+    [Area("Admin")]
+    public class SendEmailToUserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+        public SendEmailToUserController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostSendEmailToUser(string? subject, string? message)
+        {
+            if (message == null || subject == null)
+            {
+                return NotFound();
+            }
+            var users = await _context.Users.Where(u => u.UserRole == UserRole.NormalUser).ToListAsync();
+            var sendEmailToUser = new SendEmailToUser(_configuration);
+            var count = 0;
+            foreach (var user in users)
+            {
+                var result = await sendEmailToUser.SendEmail(user.Email, subject, message);
+                if (result)
+                {
+                    count += 1;
+                }
+            }
+            TempData["SuccessMessage"] = $"پیام با موفقیت به {count} کاربر ارسال شد.";
+            return RedirectToAction("Index", "SendEmailToUser", new { area = "Admin" });
+        }
+    }
+}
---
+++ PowerBlog.Site/Utilities/SendEmailToUser.cs
@@ -0,0 +1,54 @@
+﻿using MailKit.Security;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace PowerBlog.Site.Utilities
+{
+    public class SendEmailToUser
+    {
+        private readonly IConfiguration _configuration;
+        public SendEmailToUser(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<bool> SendEmail(string email, string subject, string userMessage)
+        {
+            var emailSettings = _configuration.GetSection("EmailSettings");
+            string smtpServer = emailSettings["SmtpServer"]!;
+            int port = int.Parse(emailSettings["Port"]!);
+            string senderEmail = emailSettings["SenderEmail"]!;
+            string senderPassword = emailSettings["SenderPassword"]!;
+            bool enableSSL = bool.Parse(emailSettings["EnableSSL"]!);
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("PowerBlog", senderEmail));
+
+            string recipientEmail = email;
+            message.To.Add(new MailboxAddress("", recipientEmail));
+
+            message.Subject = $"{subject}";
+            message.Body = new TextPart("None")
+            {
+                Text = userMessage
+            };
+            try
+            {
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync(smtpServer, port, SecureSocketOptions.StartTls);
+
+                    await client.AuthenticateAsync(senderEmail, senderPassword);
+
+                    await client.SendAsync(message);
+
+                    await client.DisconnectAsync(true);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
